b0bb83a48ca159ac6dc8dcb9ac562addb74fe411
Merveille <merveille.m@hotmail.fr>
2023-11-27 10:43:42 +0100

    Q.1

---
+++ TPMajInfo1.c
@@ -0,0 +1,20 @@
+#include <unistd.h> // For write
+#include <stdlib.h> // For exit
+#include <string.h> // For strlen
+
+#define WELCOME_MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
+#define PROMPT "enseash % "
+
+void display(char * message){
+    write(STDOUT_FILENO, message, strlen(message));
+}
+
+int main() {
+    display(WELCOME_MESSAGE);
+    while (1) {
+        display(PROMPT);
+        break;
+    }
+
+    return 0;
+}
